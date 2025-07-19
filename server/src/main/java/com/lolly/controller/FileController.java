58f34783e2b7adef6dc4cc8558b92161b9c1b8ac
Dylan Lolly <dylan.e.lolly@gmail.com>
2025-07-19 07:30:32 -0400

    Added fileController

---
+++ server/src/main/java/com/lolly/controller/FileController.java
@@ -0,0 +1,32 @@
+package com.lolly.controller;
+
+// THIS CODE IS BORROWED FROM GEEKSFORGEEKS.ORG
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Controller
+@RequestMapping("/images")
+public class FileController {
+
+    // Endpoint to serve image file
+    @GetMapping("")
+    public ResponseEntity<Resource> getImage() throws Exception {
+        // Path to the image file
+        Path path = Paths.get("src/main/resources/static/images/imageexample.png");
+        // Load the resource
+        Resource resource = new UrlResource(path.toUri());
+        // Return ResponseEntity with image content type
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(resource);
+    }
+}
